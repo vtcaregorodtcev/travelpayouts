53851036f1baf8ca761cb5b6ccc9fa5c1769dbf9
Vadim Tcaregorodtcev <dev.tcaregorodtcev@gmail.com>
2020-03-21 00:15:37 +0300

    add dark mode

--- src/app.svelte
+++ src/app.svelte
@@ -2,7 +2,11 @@
   import { __ } from "./i18n.js";
   import Container from "./components/container.svelte";
 
-  let locale = "en";
+  $: url = new URL(window.location);
+  $: params = new URLSearchParams(url.search);
+
+  $: locale = params.get("locale") || "en";
+  $: theme = params.get("theme") || "";
 
   $: dict = __(locale);
 </script>
@@ -28,8 +32,13 @@
     --size-256: 256px;
     --size-200: 200px;
   }
+
+  .travelpayouts.dark {
+    --color-bg: #333;
+    --color-accent: #498fe1;
+  }
 </style>
 
-<div class="travelpayouts">
+<div class="travelpayouts {theme}">
   <Container {locale} {dict} onLocaleChange={l => (locale = l)} />
 </div>
